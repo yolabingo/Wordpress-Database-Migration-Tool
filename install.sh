94947536964097eed95993e9424dfa78d71e97c2
Todd J <yolabingo@gmail.com>
2011-03-12 11:44:10 -0700

    Remove .htaccess creation

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # fix perms and create symlinks
 
-echo "Options \n  +FollowSymLinks" > .htaccess
+# echo "Options \n  +FollowSymLinks" > .htaccess
 ln -fs move-wordpress.php index.php
 ln -fs move-wordpress-cli.php do-move-wordpress-cli.php 
 
@@ -9,4 +9,3 @@ chmod 755 .
 chmod 644  favicon.png  grey-s.png  index.php  move-wordpress.inc.php  move-wordpress.php .htaccess
 chmod 600 README
 chmod 700 install.sh  move-wordpress-cli.php 
-
