aa9f187c1cd26054b47a1622fd131ca8bfcff5e7
Todd J <yolabingo@gmail.com>
2011-03-13 01:47:11 -0700

    GPL'd

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-# fix perms and create symlinks
 
-# echo "Options \n  +FollowSymLinks" > .htaccess
+# Try this if the index file is ignored...
+# echo "Options" >> .htaccess
+# echo "  +FollowSymLinks" >> .htaccess
+# chmod 644 .htaccess
+
+# create symlinks
+
 ln -fs move-wordpress.php index.php
 ln -fs move-wordpress-cli.php do-move-wordpress-cli.php 
 
+# set perms that should work for many situations 
+
 chmod 755 .
-chmod 644  favicon.png  grey-s.png  index.php  move-wordpress.inc.php  move-wordpress.php .htaccess
+chmod 644  favicon.png  grey-s.png  move-wordpress.inc.php  move-wordpress.php
 chmod 600 README
 chmod 700 install.sh  move-wordpress-cli.php 
